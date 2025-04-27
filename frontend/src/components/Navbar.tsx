075342d5842ed72c30c886d09718f606e1a1a00c
smartstoxvest-org-admin <smartstoxvest@gmail.com>
2025-04-27 12:21:56 +0100

    Update Navbar.tsx

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,13 +1,20 @@
-import React from 'react';
-import Header from './Header';
-import Navigation from './Navigation';
+import React from "react";
+import Header from "./Header";
+import Navigation from "./Navigation";
 
 const Navbar: React.FC = () => {
   return (
-    <div className="bg-white dark:bg-gray-900 shadow-md sticky top-0 z-50">
-      <Header />
-      <Navigation />
-    </div>
+    <header className="bg-white dark:bg-gray-900 shadow-md sticky top-0 z-50 w-full">
+      {/* Top Header */}
+      <div className="px-4 py-2 flex justify-between items-center border-b dark:border-gray-700">
+        <Header />
+      </div>
+
+      {/* Navigation Links */}
+      <div className="px-4 py-2">
+        <Navigation />
+      </div>
+    </header>
   );
 };
 
