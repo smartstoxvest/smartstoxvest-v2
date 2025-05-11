77a55928db3a828d4b33a08390bfa478b0237589
smartstoxvest-org-admin <smartstoxvest@gmail.com>
2025-05-11 18:06:10 +0100

    Update App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,14 @@
-
 import AppRoutes from "./AppRoutes";
+import { Toaster } from "react-hot-toast";
 
 function App() {
-  return <AppRoutes />;
+  return (
+    <>
+      <Toaster position="top-right" />
+      <AppRoutes />
+    </>
+  );
 }
 
 export default App;
+
